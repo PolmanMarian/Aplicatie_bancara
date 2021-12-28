384f594b73464d098c5b8eb01969bc68d7e5397e
nedecuioanandrei <anedelcu95@yahoo.com>
2021-12-28 18:53:13 +0200

    update

--- Interfata_grafica/src/Main.java
+++ Interfata_grafica/src/Main.java
@@ -1,7 +1,13 @@
 import javax.swing.*;
+import java.sql.*;
 
 public class Main {
     /// urmeaza sa facem conexiunea
+    private final static String URL = "jdbc:mysql://localhost:3306/";
+    private final static String DB_NAME = "Aplicatie_bancara";
+    private final static String USER = "root";
+    private final static String PASSWORD = "fronemaneroot";
+
     public static JFrame currentFrame;
     public static void changeCurrentFrame(JFrame newFrame){
         currentFrame = newFrame;
@@ -9,6 +15,19 @@ public class Main {
     }
 
     public static void main(String ... argc) {
-        changeCurrentFrame(new MainFrame("Aplicatie Bancara"));
+        Connection c;
+        try {
+            c = DriverManager.getConnection(URL + DB_NAME , USER , PASSWORD);
+            PreparedStatement s = c.prepareStatement("select * from user_rank");
+            ResultSet rez = s.executeQuery();
+            while(rez.next()) {
+                System.out.println("Rank " + rez.getString("rank"));
+            }
+        } catch ( Exception e ) {
+            System.out.println("Nu -i bine!");
+            System.out.println("SQLState: " + ((SQLException) e).getSQLState());
+            System.out.println("VendorError: " + ((SQLException) e).getErrorCode());
+        }
+        //changeCurrentFrame(new MainFrame("Aplicatie Bancara"));
     }
 }
